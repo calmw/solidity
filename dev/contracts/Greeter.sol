0045faa5322d27dd34fb5b16f0e5eb5d51979a10
Wang fei <calm.wang@hotmail.com>
2023-04-13 10:41:28 +0800

    映射迭代

--- dev/contracts/Greeter.sol
+++ dev/contracts/Greeter.sol
@@ -6,7 +6,7 @@ contract MappingIterator {
     string[] keys;
 
     // 给map添加键值对（key不存在添加键值对，存在更新值）
-    function put(string key, address addr)returns (bool)  {
+    function put(string calldata key, address addr) public returns (bool)  {
         bool exists = elements[key] != address(0);
         if (!exists) {//不存在
             keys.push(key);
@@ -15,7 +15,27 @@ contract MappingIterator {
         return true;
     }
 
-    function getKeyCount() constant returns (uint) {
+    function getKeyCount() public view returns (uint) {
         return keys.length;
+    }
+
+    // 使用时，防止索引越界
+    function getElementAtIndex(uint index) public view returns (address)  {
+        return elements[keys[index]];
+    }
+
+    function getElement(string calldata key) public view returns (address)  {
+        return elements[key];
+    }
+
+    function delElement(string calldata key) public {
+        for (uint i; i < keys.length; i++) {
+            string memory ki = keys[i];
+            if (keccak256(abi.encodePacked(key)) == keccak256(abi.encodePacked(ki))) {
+                delete elements[key];
+                delete keys[i];
+                return;
+            }
+        }
     }
 }
