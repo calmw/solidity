daf41703922eba325a12b8104824c225fe4b9511
Wang fei <calm.wang@hotmail.com>
2023-04-13 11:54:56 +0800

    节省Gas

--- dev/contracts/Greeter.sol
+++ dev/contracts/Greeter.sol
@@ -2,39 +2,10 @@
 pragma solidity ^0.8.0;
 
 contract MappingIterator {
-    mapping(string => address)elements;
-    string[] keys;
-
-    // 给map添加键值对（key不存在添加键值对，存在更新值）
-    function put(string calldata key, address addr) public returns (bool)  {
-        bool exists = elements[key] != address(0);
-        if (!exists) {//不存在
-            keys.push(key);
-        }
-        elements[key] = addr;
-        return true;
-    }
-
-    function getKeyCount() public view returns (uint) {
-        return keys.length;
-    }
-
-    // 使用时，防止索引越界
-    function getElementAtIndex(uint index) public view returns (address)  {
-        return elements[keys[index]];
-    }
-
-    function getElement(string calldata key) public view returns (address)  {
-        return elements[key];
-    }
-
-    function delElement(string calldata key) public {
-        for (uint i; i < keys.length; i++) {
-            if (keccak256(abi.encodePacked(key)) == keccak256(abi.encodePacked(keys[i]))) {
-                delete elements[key];
-                delete keys[i];
-                return;
-            }
-        }
-    }
+    //1 uint8 可以直接用uint256类型，如果在结构体里，优先考虑是否可以和其他变量内存对齐
+    // 2
+    uint256 val = storageData;
+    uint256 memoryData = storageData;
+    // 此处省略memoryData参与的各种逻辑，最后将memoryData再赋值给val（storage data）
+    uint256 val = memoryData;
 }
